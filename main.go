690de5628e64f707fa4f240bf4f36ded82ba7f00
Raj Perera <raj.perera@ubisoft.com>
2019-04-05 18:18:30 -0400

    Fix issue with template

--- main.go
+++ main.go
@@ -6,14 +6,38 @@ import (
 	"html/template"
 	"log"
 	"os"
+	"strings"
+	"regexp"
 	"path/filepath"
 )
+
+type FuncMap map[string]interface{}
+
+var AlertManagerFuncs = FuncMap{
+	"toUpper": strings.ToUpper,
+	"toLower": strings.ToLower,
+	"title":   strings.Title,
+	// join is equal to strings.Join but inverts the argument order
+	// for easier pipelining in templates.
+	"join": func(sep string, s []string) string {
+		return strings.Join(s, sep)
+	},
+	"match": regexp.MatchString,
+	"safeHtml": func(text string) template.HTML {
+		return template.HTML(text)
+	},
+	"reReplaceAll": func(pattern, repl, text string) string {
+		re := regexp.MustCompile(pattern)
+		return re.ReplaceAllString(text, repl)
+	},
+}
 
 func main() {
 	var (
 		flTemplate       = flag.String("template", "", "path to template")
 		flJSON           = flag.String("json", "", "json input to use in template")
 		flTemplateString = flag.Bool("string", false, "template is a string, not a file")
+		flEnableAlertmanager = flag.Bool("alertmanager", false, "enable alertmanager template functions")
 	)
 	flag.Parse()
 
@@ -28,13 +52,21 @@ func main() {
 		name string
 	)
 
+	name = "template"
+	tmpl = template.New(name)
+
+	if *flEnableAlertmanager {
+		tmpl = tmpl.Funcs(template.FuncMap(AlertManagerFuncs))
+	}
+
 	if *flTemplateString {
-		name = "template"
-		tmpl, err = template.New(name).Parse(*flTemplate)
+		tmpl, err = tmpl.Parse(*flTemplate)
 	} else {
 		name = filepath.Base(*flTemplate)
-		tmpl, err = template.ParseFiles(*flTemplate)
+		tmpl, err = tmpl.ParseFiles(*flTemplate)
 	}
+
+
 	if err != nil {
 		log.Fatal(err)
 	}
